182588006aa8d8cf0252422c62f78289a45ba634
MonGen2211 <hosonvu02@gmail.com>
2025-07-16 18:01:19 +0700

    upload to deploy

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+define('APP_NAME', 'Godashop');
+define('host', 'metro.proxy.rlwy.net');
+define('port', '43484');
+define('SERVERNAME', 'localhost');
+define('username', 'root');
+define('password', 'XXmcqUQLdxryUcBcVdlRODCMRGFlAhPm');
+define('dbname', 'railway');
+
+// SMTP (send mail)
+define("SMTP_USERNAME", "nguyenthuchanh2021@gmail.com");
+define("SMTP_SECRET", "howrfxnasqiuybxy");
+define("SMTP_HOST", "smtp.gmail.com");
+define("SHOP_OWNER", "nguyenhuulocla2006@gmail.com");
+
+// JWT key
+define("JWT_KEY", "con bò đang gặm cỏ sau hè");
+
+define("GOOGLE_RECAPTCHA_SITE", "6Lcj07oUAAAAALAHcj_WdDa7Vykqzui3mSA5SIoe");
+define("GOOGLE_RECAPTCHA_SECRET", "6Lcj07oUAAAAAKKbrzK72AzMqqVwxnsVTjlvx_OJ");
